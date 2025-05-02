0641094fa4f375b5f6ec06f8ad4f701359bf0705
sravya <sravya.rani143@gmail.com>
2025-05-02 23:18:34 +0530

    Circuit breaker with resilience4J

---
+++ src/main/java/com/learning/microservices/currency_exchange/controller/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package com.learning.microservices.currency_exchange.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name="sample-api", fallbackMethod = "sendHardCodedResponse")
+    public String sampleApi(){
+        logger.info("Sample API call receieved");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-notworking/",String.class);
+        return forEntity.getBody();
+    }
+
+    public String sendHardCodedResponse(Exception ex){
+        return "Hard coded response";
+    }
+}
